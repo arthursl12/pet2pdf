56815ccd3b9ffb936d9c5ba9a0b53e74ddd225f9
arthursl12 <arthursoutolima@gmail.com>
2020-07-08 12:33:27 -0300

    Conversão - O último ponto da conversão requer dev no Windows

--- pet2pdf.py
+++ pet2pdf.py
@@ -50,9 +50,42 @@ def defNomeArquivo():
         if not userOK: print()
     return fileName
 
-def main():
-    preffix = defNomeArquivo()
+# Dado o nome de um arquivo com uma extensão qualquer, retira-se a extensão
+# Retorna uma nova string com o nome do arquivo sem a extensão
+def extArquivo(nome):
+    # Assegurar que é uma string
+    nome = str(nome)
 
+    # Encontrar o índice do '.' 
+    for i in range(len(nome)):
+        j = len(nome) - i - 1
+        if nome[j] == '.':
+            break
+    
+    # Particiona o nome até antes do '.'
+    res = nome[0:j]
+    return res
+
+def main():
+    # preffix = defNomeArquivo()
+    
+
+    # CONVERSÃO DE DOCX PARA PDF
+    # Iterar pelos arquivos da pasta 'docs'
+    docsDir = next(os.walk('./docs'))
+    docsList = docsDir[2]
+    for file in docsList:
+        nome = extArquivo(file)
+        nomeDOCX = str(file)
+        nomePDF = nome + '.pdf'
+
+        path = os.path.abspath('docs/')
+        pathDOCX = path + nomeDOCX
+        pathPDF = path + nomePDF
+
+        # docx2pdf.convert(pathDOCX, pathPDF)
+        # print(os.path.abspath('docs/'+file))
+        # print(file)
 
     
 
