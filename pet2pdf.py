31e2b04aea72300f2ea7629faf580de41ace1c16
arthursl12 <arthursoutolima@gmail.com>
2020-07-08 08:48:08 -0300

    Refatoração e confirmação do usuário

--- pet2pdf.py
+++ pet2pdf.py
@@ -11,25 +11,51 @@ def completaZero(num):
     res = '0' + str(num)
     return res
 
+# Apresenta uma seção de confirmação para o usuário, que digita Y ou N
+# para confirmar ou negar
+def userConfirm():
+    validInput = False
+    while (not validInput):
+        userVal = input('Confirmar? (S/N) ')
+        if len(userVal) != 1: continue
+        userVal = userVal.upper()
+        if userVal != str('S') and userVal != str('N'): continue
+        if userVal == 'S':
+            validInput = True
+            return True
+        elif userVal == 'N':
+            validInput = True
+            return False
+
+def defNomeArquivo():
+    userOK = False
+    while (not userOK):
+        ## DEFINIÇAO DO NOME DO ARQUIVO
+        # Data
+        today = date.today()
+        month = completaZero(today.month)
+        day = completaZero(today.day)
+        
+        # Última posição já utilizada
+        lastNum = input('Ultimo numero da ordem: ')
+        while (not lastNum.isdigit()):
+            print('Entrada nao eh um numero valido')
+            lastNum = input('Ultimo numero da ordem: ')
+        currentNum = int(lastNum) + 1
+        
+        # Formação do prefixo do nome dos arquivos
+        fileName = str(currentNum) + '. ' + day + '-' + month + ' / '
+        print('Arquivos terao nome: ', fileName)
+        userOK = userConfirm()
+        if not userOK: print()
+    return fileName
 
 def main():
-    ## DEFINIÇAO DO NOME DO ARQUIVO
-    # Data
-    today = date.today()
-    month = completaZero(today.month)
-    day = completaZero(today.day)
+    preffix = defNomeArquivo()
+
+
     
-    # Última posição já utilizada
-    lastNum = input('Ultimo numero da ordem: ')
-    while (not lastNum.isdigit()):
-        print('Entrada nao eh um numero valido')
-        lastNum = input('Ultimo numero da ordem: ')
-    currentNum = int(lastNum) + 1
-    
-    # Formação do prefixo do nome dos arquivos
-    fileName = str(currentNum) + '. ' + day + '-' + month + ' / '
-    print('Arquivos terao nome: ', fileName)
-    
+
 
 
 
